43567312ae8690e7ac8f959ff05094e94ffeb3f2
jedbillyb <jedblenkhorn@gmail.com>
2025-05-02 12:57:18 +1200

    Fixed chosen activity assign and added final output print

--- AS92004-1.1.py
+++ AS92004-1.1.py
@@ -34,6 +34,7 @@ def get_age():
             print('Invalid input. Please enter a valid number for age.')
             print()
 
+age = get_age()
 
 def validate_age(age):
     if age < 5:
@@ -50,43 +51,36 @@ validate_age(get_age())
 print()
 
 def get_chosen_activity():
-
-    activity_list = ['Cultural immersion. This is for 5 days and is considers "easy" and cost $800.','Kayaking and pancakes. This is for 3 days and is considered "moderate" and costs $400.','Mountain biking. This is for 4 days and is considered "difficult and cost $900.']
+    activity_list = [
+        'Cultural immersion. This is for 5 days and is considered "easy" and costs $800.',
+        'Kayaking and pancakes. This is for 3 days and is considered "moderate" and costs $400.',
+        'Mountain biking. This is for 4 days and is considered "difficult" and costs $900.'
+    ]
     
     print('Please choose an activity from the list below:')
     print()
-    print(f'1. {activity_list[0]} ')
-    print(f'2. {activity_list[1]} ')
-    print(f'3. {activity_list[2]} ')
-
+    print(f'1. {activity_list[0]}')
+    print(f'2. {activity_list[1]}')
+    print(f'3. {activity_list[2]}')
 
     while True:
         try:
-            chosen_activity = int(input('Please enter the number corresponding to the activity you wish to particpate in : '))
-            if chosen_activity in [1]:
-                activity = ('Cultural immersion')
-                day_length = 5
-                difficulty = ('easy')
-                cost = 800
-                return activity, day_length, difficulty, cost
-            elif chosen_activity in [2]:
-                activity = ('Kayaking and pancakes')
-                day_length = 3
-                difficulty = ('moderate')
-                cost = 400
-                return activity, day_length, difficulty, cost
-            elif chosen_activity in [3]:
-                activity = ('Mountain biking')
-                day_length = 4
-                difficulty = ('difficult')
-                cost = 900
-                return activity, day_length, difficulty, cost
+            chosen_activity = int(input('Please enter the number corresponding to the activity you wish to participate in: '))
+            if chosen_activity == 1:
+                return 'Cultural immersion', 5, 'easy', 800
+            elif chosen_activity == 2:
+                return 'Kayaking and pancakes', 3, 'moderate', 400
+            elif chosen_activity == 3:
+                return 'Mountain biking', 4, 'difficult', 900
             else:
                 print('Invalid choice. Please enter a number between 1 and 3.')
                 print()
         except ValueError:
             print('Invalid input. Please enter a number corresponding to your chosen activity.')
             print()
+
+# Call the function and unpack the returned values
+activity, day_length, difficulty, cost = get_chosen_activity()
             
 chosen_activity = get_chosen_activity()
 
@@ -123,3 +117,5 @@ def get_shuttle_bus():
             print()
 
 shuttle_bus = get_shuttle_bus()
+
+print(f'Hello {name}, you are {age} years old. you are going to participate in {activity} which is {day_length} days long and is considered {difficulty} and costs ${cost}. Your meal preference is {meal_preference} and you have chosen to {shuttle_bus} the shuttle bus.')
