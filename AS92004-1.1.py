f611f5abefd0285bb592cb91bf247c7465fa2a73
jedbillyb <jedblenkhorn@gmail.com>
2025-05-06 09:14:37 +1200

    Changed camper name and age functions for clarity

--- AS92004-1.1.py
+++ AS92004-1.1.py
@@ -9,8 +9,7 @@
 activity_list = [
 'Cultural immersion. This is for 5 days and is considered "easy" and costs $800.',
 'Kayaking and pancakes. This is for 3 days and is considered "moderate" and costs $400.',
-'Mountain biking. This is for 4 days and is considered "difficult" and costs $900.'
-]
+'Mountain biking. This is for 4 days and is considered "difficult" and costs $900.']
 # 2. List of meal preferences
 meal_list = ['Standard', 'Vegetarian', 'Vegan']
 
@@ -26,31 +25,31 @@ print('Welcome to the holiday camp!')
 
 print()
 
-def get_name():
+def def_camper_name():
     while True:
-        name = input('Please enter your name: ')
-        if len(name) > 0:
-            return name
+        camper_name = input('Please enter your name: ')
+        if len(camper_name) > 0:
+            return camper_name
         else:
             print('Name cannot be empty. Please try again.')
             print()
 
-name = get_name()
+camper_name = def_camper_name()
 
 print()
 
-def get_age(min_age, max_age):
+def def_camper_age(min_age, max_age):
     while True:
         try:
-            age = int(input(f'Hi {name}, how old are you? '))
-            if age < 5:
+            camper_age = int(input(f'Hi {camper_name}, how old are you? '))
+            if camper_age < min_age:
                 print('Sorry, you are too young to attend the camp.')
                 exit()
-            elif age > 18:
+            elif camper_age > max_age:
                 print('Sorry, you are too old to attend the camp.')
                 exit()
-            elif 5 <= age <= 18:
-                return age
+            elif min_age <= camper_age <= max_age:
+                return camper_age
             else:
                 print('Age must be a positive number. Please try again.')
                 print()
@@ -58,12 +57,12 @@ def get_age(min_age, max_age):
             print('Invalid input. Please enter a valid number for age.')
             print()
 
-age = get_age(min_age, max_age)
+camper_age = def_camper_age(min_age, max_age)
 
 
 print()
 
-def get_chosen_activity(activity_list):
+def def_activity_choice(activity_list):
     
     print('Please choose an activity from the list below:')
     print(f'1. {activity_list[0]}')
@@ -74,12 +73,12 @@ def get_chosen_activity(activity_list):
 
     while True:
         try:
-            chosen_activity = int(input('Please enter the number corresponding to the activity you wish to participate in: '))
-            if chosen_activity == 1:
+            activity_choice = int(input('Please enter the number corresponding to the activity you wish to participate in: '))
+            if activity_choice == 1:
                 return 'Cultural immersion', 5, 'easy', 800
-            elif chosen_activity == 2:
+            elif activity_choice == 2:
                 return 'Kayaking and pancakes', 3, 'moderate', 400
-            elif chosen_activity == 3:
+            elif activity_choice == 3:
                 return 'Mountain biking', 4, 'difficult', 900
             else:
                 print('Invalid choice. Please enter a number between 1 and 3.')
@@ -89,7 +88,7 @@ def get_chosen_activity(activity_list):
             print()
 
 # Call the function and unpack the returned values
-activity, day_length, difficulty, cost = get_chosen_activity(activity_list)
+chosen_activity, day_length, difficulty, cost = def_activity_choice(activity_list)
 
 def get_meal_preference(meal_list):
 
@@ -144,7 +143,7 @@ final_cost = cost + aditional_cost
 
 
 print()
-print(f'Hello {name}, you are {age} years old. you are going to participate in {activity} which is {day_length} days long and is considered {difficulty} and costs ${cost}. Your meal preference is {meal_choice} and you have chosen to {shuttle_bus} the shuttle bus. Your total cost is ${final_cost}.')
+print(f'Hello {camper_name}, you are {camper_age} years old. you are going to participate in {chosen_activity} which is {day_length} days long and is considered {difficulty} and costs ${cost}. Your meal preference is {meal_choice} and you have chosen to {shuttle_bus} the shuttle bus. Your total cost is ${final_cost}.')
 print()
 
 final = input(f'Would you like to confirm your booking at the cost of {final_cost}? (yes/no) (y/n)')
